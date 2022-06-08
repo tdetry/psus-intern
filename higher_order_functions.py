1d0c2510075ae792140fb98eff34da2df43779ff
teth-odoo <teth@odoo.com>
2022-06-08 11:26:24 -0400

    Decorators
    Signed-off-by: teth-odoo <teth@odoo.com>

--- higher_order_functions.py
+++ higher_order_functions.py
@@ -9,5 +9,15 @@ def operation(func,x):
     return res
 
 print(operation(inc,1))
-
 print(operation(dec,1))
+
+def is_called():
+    def is_returned():
+        print("Hello")
+    return is_returned
+
+
+new = is_called()
+
+# Outputs "Hello"
+new()

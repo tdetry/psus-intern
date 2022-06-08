3c5a5364013941157339ab1ce2fd016ed8ceca9a
suju-odoo <suju-odoo@odoo.com>
2022-06-08 14:36:46 -0400

    second commit

---
+++ app.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+        
+class Solution:
+    # two binary trees are considered the same if they are identical
+    #
+    def isSameTree(self, p: Optional[TreeNode], q : Optional[TreeNode]) -> bool: 
+        if p == None and q == None : 
+            return True 
+        elif p == None or q == None:
+            return False  
+        else : 
+            if p.val != q.val :
+                return False
+                
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range( len (matrix) ):
+            if target < matrix[i][0]:
+                return False
+            else :
+                for j in range ( len ( matrix[0] ) ): 
+                    if target == matrix[i][j]:
+                        return True 

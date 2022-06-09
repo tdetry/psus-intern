1ec5bfe147d236c5005098aa7aff5ba327f8b214
Alexa Acosta <alac@odoo.com>
2022-06-09 10:12:25 -0400

    starting js exercises

---
+++ exercises.js
@@ -0,0 +1,9 @@
+var a; // declaration
+var b = 7; // assignment
+a = b;
+
+var c = 7;
+c = "value" // value types can change for var
+/* var   : usable variable anywhere
+   let   : local variable scope
+   const : unchanging variable */
